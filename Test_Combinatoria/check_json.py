37a4c36742f01351d44967c081ec063b3dc25b18
Pabloe <pabloecancino@gmail.com>
2025-12-25 15:37:20 -0700

    chore: add .gitignore to exclude work documents from public repo

--- Test_Combinatoria/check_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('configuraciones_nudos_k3.json', 'r') as f:
-    data = json.load(f)
-
-print("Primeras 5 configuraciones del JSON:")
-for d in data[:5]:
-    print(f"ID {d['id']}: {d['configuracion_Racional']}")
